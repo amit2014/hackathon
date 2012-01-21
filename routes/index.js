646bcd6e0dbdb3f12d54b7ead8bdc361440cd450
Paul Xue <xuepaul.xp@gmail.com>
2012-01-20 20:05:47 -0500

    scraping test

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,25 @@
  */
 
 exports.index = function(req, res){
+  var httpAgent = require('http-agent');
+  var jsdom = require('jsdom'),
+      sys = require('sys');
+      util = require('util');
+
+  var agent = httpAgent.create('www.google.com', ['finance', 'news', 'images']);
+
+  agent.addListener('next', function(err, agent){
+    console.log('body of the current page', agent.body);
+    console.log('Response of the current page', util.inspect(agent.respnse));
+
+    agent.next();
+  });
+
+  agent.addListener('stop', function(err, agent){
+    console.log('agent has stopped');
+  });
+
+  agent.start();
+
   res.render('index', { title: 'Express' })
 };
